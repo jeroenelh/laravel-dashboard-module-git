833e58590395b6e23bfb5fc816aa8e4672c259d8
Jeroen el Hahaoui <jeroen.el.hahaoui@maxict.nl>
2023-07-29 21:26:34 +0200

    Update migration

--- src/database/migrations/2023_07_29_000003_create_git_pull_requests_table.php
+++ src/database/migrations/2023_07_29_000003_create_git_pull_requests_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
         Schema::create('git_pull_requests', function (Blueprint $table) {
             $table->bigInteger('id');
             $table->string('source');
+            $table->string('title');
             $table->integer('number');
             $table->string('state');
             $table->string('repository_id');
